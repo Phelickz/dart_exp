4767c89d24e52bd8c4506efb42bd6a6f62569758
felixhope30@gmail.com <felixhope30@gmail.com>
2021-06-13 20:24:01 +0100

    bump

--- lib/src/model/get.dart
+++ lib/src/model/get.dart
@@ -5,19 +5,20 @@ import '../../express_dt.dart';
 // import '../../express_dt.dart';
 
 class ExpressMethod {
+  //request route
   String? route;
+
+  //Function to be initialized in the route. Leave it blank.
   Map<String, List<Function(ExpressRequest req, ExpressResponse res)>>
       callback;
+
+  //lists of callback functions. Can be middlewares, or just one request function.
   List<Function(ExpressRequest, ExpressResponse)>? callbacks;
-  String? session;
-  Function()? next;
 
   ExpressMethod({
     Map<String, List<Function(ExpressRequest req, ExpressResponse res)>>?
       callback,
     this.route,
     this.callbacks,
-    this.session,
-    this.next,
   }) : callback = callback ?? {}, assert(callback != null);
 }
